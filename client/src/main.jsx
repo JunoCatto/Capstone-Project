db24d6afdf145990695cc0dea4642c6f110f6ed3
Juno <owen.g.howells@gmail.com>
2025-12-12 16:52:44 +1100

    fixed package.json

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 
 // context
-import { AuthProvider } from "./context/AuthContext.jsx";
+import { AuthProvider } from "./context/authcontext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
