2a7c6361b57727371a39b3030911223a6cc2ae07
Juno <owen.g.howells@gmail.com>
2026-01-07 13:39:15 +1100

    blah

--- server/server.js
+++ server/server.js
@@ -6,7 +6,12 @@ import cors from "cors";
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://capstone-project-frontend-767r.onrender.com",
+    credentials: true,
+  })
+);
 
 // landing page
 app.get("/", (req, res) => {
@@ -24,5 +29,5 @@ app.use((req, res) => {
 });
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server is running");
+  console.log("Server is running on port", PORT);
 });
